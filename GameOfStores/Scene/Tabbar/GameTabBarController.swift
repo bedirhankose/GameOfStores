a612a357f6603b3c580456cf517c5c332d7b204b
Bedirhan Köse <bedirhan.kose05@gmail.com>
2023-01-24 15:53:42 +0100

    Tabbar controller

---
+++ GameOfStores/Scene/Tabbar/GameTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  GameTabBarController.swift
+//  GameOfStores
+//
+//  Created by Bedirhan Köse on 24.01.23.
+//
+
+import UIKit
+
+class GameTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpTabBar()
+        
+    }
+}
