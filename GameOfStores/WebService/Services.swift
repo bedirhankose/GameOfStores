3a928240758f322dd5179447cbdac6f6bb0a71c5
Bedirhan Köse <bedirhan.kose05@gmail.com>
2023-01-24 14:18:49 +0100

    Services Final class created.

--- GameOfStores/WebService/Services.swift
+++ GameOfStores/WebService/Services.swift
@@ -13,3 +13,27 @@ protocol ServiceProtocol {
     func fetchGames(completion: @escaping ([GameResult]?) -> Void)
     func getGameDetail(gameId: Int, completion: @escaping (GameDetailResults?) -> Void)
 }
+
+// MARK: - Services
+final class Services: ServiceProtocol {
+    
+    func fetchGames(completion: @escaping ([GameResult]?) -> Void) {
+        AF.request(Constant.NetworkConstant.GameServiceEndPoint.fetchGame()).responseDecodable(of: GameListModel.self) { data in
+            guard let data = data.value else {
+                completion(nil)
+                return
+            }
+            completion(data.results)
+        }
+    }
+    
+    func getGameDetail(gameId: Int, completion: @escaping (GameDetailResults?) -> Void) {
+        AF.request(Constant.NetworkConstant.GameServiceEndPoint.detailGame(gameId: gameId)).responseDecodable(of: GameDetailResults.self) { data in
+            guard let data = data.value else {
+                completion(nil)
+                return
+            }
+            completion(data)
+        }
+    }
+}
