fa020d39727b5e8a51b57e9118cd06acf87b63d5
Bedirhan Köse <bedirhan.kose05@gmail.com>
2023-01-24 14:36:06 +0100

    getRequest

--- GameOfStores/CoreData/Persistance/CoreData/Manager/CoreDataManager.swift
+++ GameOfStores/CoreData/Persistance/CoreData/Manager/CoreDataManager.swift
@@ -8,7 +8,23 @@
 import Foundation
 import CoreData
 
-final class {
+final class CoreDataManager {
     
+    static let shared = CoreDataManager()
     
+    var coreDataStack: CoreDataStack
+    
+    var moc: NSManagedObjectContext {
+        return coreDataStack.managedContext
+    }
+    
+    private init(coreDataStack: CoreDataStack = CoreDataStack(modelName: "GameOfStores")) {
+        self.coreDataStack = coreDataStack
+    }
+    
+    private func getRequest() -> NSFetchRequest<Game> {
+        let request:NSFetchRequest<Game> = Game.fetchRequest()
+        request.returnsObjectsAsFaults = false
+        return request
+    }
 }
