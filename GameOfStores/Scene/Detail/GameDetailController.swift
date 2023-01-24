7803a692aa5efd499294cc13daaa7a3ef0b42525
Bedirhan Köse <bedirhan.kose05@gmail.com>
2023-01-24 15:26:25 +0100

    Properties

---
+++ GameOfStores/Scene/Detail/GameDetailController.swift
@@ -0,0 +1,27 @@
+//
+//  GameDetailController.swift
+//  GameOfStores
+//
+//  Created by Bedirhan Köse on 24.01.23.
+//
+
+import UIKit
+
+class GameDetailController: UIViewController {
+
+    // MARK: - Properties
+    private var gameDetailResults: GameDetailResults
+    var gameID: Int?
+    var errorMessage = String()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
