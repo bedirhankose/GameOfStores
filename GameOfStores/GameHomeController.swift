de653bcebf8ea964a161400c77d7c4aee89c9d67
Bedirhan Köse <bedirhan.kose05@gmail.com>
2023-01-24 13:26:51 +0100

    renamed.

---
+++ GameOfStores/GameHomeController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameOfStores
+//
+//  Created by Bedirhan Köse on 24.01.23.
+//
+
+import UIKit
+
+class GameHomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
