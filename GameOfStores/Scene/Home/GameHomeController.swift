67b4c8d5d16508f82f2a0c009912386f72c8920c
Bedirhan Köse <bedirhan.kose05@gmail.com>
2023-01-24 15:06:54 +0100

    Viewmodel Output Protocol

---
+++ GameOfStores/Scene/Home/GameHomeController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  GameOfStores
+//
+//  Created by Bedirhan Köse on 24.01.23.
+//
+
+import UIKit
+import SnapKit
+import Kingfisher
+
+// MARK: - ViewModel Output Protocol
+protocol GameOutput {
+    func selectedGame(gameId: Int)
+}
+
+final class GameHomeController: UIViewController, GameOutput{
+    
+    func selectedGame(gameId: Int) {
+        viewModel.getGameDetail(gameId: gameId) { model in
+            guard let model = model else {
+                return
+            }
+            self.navigationController?.pushViewController(GameDetailController(gameDetailResults: model), animated: true)
+        }
+    }
+    
+    
+}
+
