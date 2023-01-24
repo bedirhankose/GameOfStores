8cc2dbe0002ceb5137af947d19ce245f7b41f325
Bedirhan Köse <bedirhan.kose05@gmail.com>
2023-01-24 16:05:22 +0100

    Constant class and related extensions.

---
+++ GameOfStores/Extensions/Constant/Game Constant/GameConstant.swift
@@ -0,0 +1,20 @@
+//
+//  GameConstant.swift
+//  GameOfStores
+//
+//  Created by Bedirhan Köse on 24.01.23.
+//
+
+import Foundation
+
+extension Constant {
+    
+    enum GameListError: String {
+        case errorLabel = "Sorry 😔 The Game not found."
+    }
+    
+    enum CellConstant: String {
+        case Identifier = "Cell"
+    }
+    
+}
