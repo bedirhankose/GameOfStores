f7917823e8d4b2baf1d2dbd320cf6f7bc61431e9
Bedirhan Köse <bedirhan.kose05@gmail.com>
2023-01-24 15:08:32 +0100

    UI Elements

--- GameOfStores/Scene/Home/GameHomeController.swift
+++ GameOfStores/Scene/Home/GameHomeController.swift
@@ -29,6 +29,49 @@ final class GameHomeController: UIViewController, GameOutput{
     private var searchData: [GameResult] = []
     private var isSearch = Bool()
     
+// MARK: - UI ELEMENTS
+    private let searchController: UISearchController = {
+        let search = UISearchController()
+        return search
+    }()
+    
+    private lazy var scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.showsHorizontalScrollIndicator = false
+        scroll.isPagingEnabled = true
+        scroll.translatesAutoresizingMaskIntoConstraints = false
+        scroll.contentMode = .scaleAspectFit
+        scroll.backgroundColor = .black
+        scroll.layer.cornerRadius = 15
+        scroll.clipsToBounds = true
+        scroll.contentSize = CGSize(width: view.frame.width * 3.6 , height: 0)
+        return scroll
+    }()
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
+    private lazy var pageController: UIPageControl = {
+        let page = UIPageControl()
+        page.numberOfPages = 3
+        page.currentPage = 0
+        page.translatesAutoresizingMaskIntoConstraints = false
+        page.pageIndicatorTintColor = UIColor.black
+        page.currentPageIndicatorTintColor = UIColor.systemRed
+        return page
+    }()
+    private lazy var errorLabel: UILabel = {
+        var label = UILabel()
+        label.font = .boldSystemFont(ofSize: 15)
+        label.textColor = .black
+        label.text = Constant.GameListError.errorLabel.rawValue
+        label.isHidden = true
+        return label
+    }()
     
     
 }
