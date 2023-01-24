c636e4b9e696fc873bd75da9f6549f02504dc668
Bedirhan Köse <bedirhan.kose05@gmail.com>
2023-01-24 15:27:25 +0100

    UIElements

--- GameOfStores/Scene/Detail/GameDetailController.swift
+++ GameOfStores/Scene/Detail/GameDetailController.swift
@@ -14,6 +14,91 @@ class GameDetailController: UIViewController {
     var gameID: Int?
     var errorMessage = String()
     
+    // MARK: - UI Elements
+    private let imageView : UIImageView = {
+        let detailImageView = UIImageView()
+        detailImageView.translatesAutoresizingMaskIntoConstraints = false
+        detailImageView.contentMode = .scaleToFill
+        detailImageView.layer.cornerRadius = 15
+        detailImageView.clipsToBounds = true
+        detailImageView.layer.borderColor = UIColor.black.cgColor
+        detailImageView.layer.borderWidth = 2
+        return detailImageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .boldSystemFont(ofSize: 25)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let metacriticLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 15)
+        label.textAlignment = .left
+        label.textColor = .systemGray
+        return label
+    }()
+    
+    private let releasedLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 15)
+        label.textAlignment = .right
+        label.textColor = .systemGray
+        return label
+    }()
+    
+    private let descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.contentInsetAdjustmentBehavior = .automatic
+        textView.isSelectable = false
+        textView.isEditable = false
+        textView.textAlignment = .left
+        textView.font = .boldSystemFont(ofSize: 20)
+        return textView
+    }()
+    
+    private let verticalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 5
+        return stackView
+    }()
+    
+    private let horizontalStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 32
+        return stackView
+    }()
+    
+    private let favoriteButton: UIButton = {
+        let button = UIButton()
+        button.tintColor = .red
+        button.setImage(UIImage(systemName: "heart"), for: .normal)
+        button.contentHorizontalAlignment = .right
+        button.contentVerticalAlignment = .top
+        button.setPreferredSymbolConfiguration(UIImage.SymbolConfiguration(pointSize: 48), forImageIn: .normal)
+        return button
+    }()
+    
+    init(gameDetailResults: GameDetailResults) {
+        self.gameDetailResults = gameDetailResults
+        super.init(nibName: nil, bundle: nil)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
