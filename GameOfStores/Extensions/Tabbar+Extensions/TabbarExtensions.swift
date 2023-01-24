cc3cbfa9f297d510a42163ef16eacf06f2e9c961
Bedirhan Köse <bedirhan.kose05@gmail.com>
2023-01-24 15:59:20 +0100

    Tabbar Setup Extensions.

---
+++ GameOfStores/Extensions/Tabbar+Extensions/TabbarExtensions.swift
@@ -0,0 +1,28 @@
+//
+//  TabbarExtensions.swift
+//  GameOfStores
+//
+//  Created by Bedirhan Köse on 24.01.23.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - Tabbar Setup Extensions
+
+extension GameTabBarController: UITabBarControllerDelegate {
+    
+    func setUpTabBar() {
+        self.tabBar.tintColor = .systemIndigo
+        self.tabBar.backgroundColor = .white
+        navigationItem.titleView?.isHidden = true
+        navigationItem.hidesBackButton = true
+        let VC1 = UINavigationController(rootViewController: GameHomeController())
+        let VC2 = UINavigationController(rootViewController: GameFavoritesController())
+        self.setViewControllers([VC1, VC2], animated: true)
+        guard let item = self.tabBar.items else { return }
+        item[0].image = UIImage(systemName: "house.fill")
+        item[1].image = UIImage(systemName: "heart.fill")
+        
+    }
+}
